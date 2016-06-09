97899a705d6eff33d18aa4b2ab2ab8f4edbf2082
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-10 01:59:54 +0200

    Rajout du body-parser dans l'index des routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,12 @@
 var fs = require('fs');
+var bodyParser  = require('body-parser');
 
 module.exports = function (app) {
     "use strict";
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
     fs.readdirSync(__dirname).forEach(function (file) {
         if (file !== "index.js") {
             var name = file.substr(0, file.indexOf('.'));
