3593e5b037e9d9a80aa6e8c656501f4a6ff04bcd
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-10 15:47:35 +0200

    Mise en place du empty-validator à la racine du routing

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,15 @@
 var fs = require('fs');
 var bodyParser  = require('body-parser');
+var morgan = require('morgan');
+var empty-validator = require('../lib/empty-validator');
 
 module.exports = function (app) {
     "use strict";
 
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+    app.use(morgan('dev'));
+    app.use(empty-validator);
 
     fs.readdirSync(__dirname).forEach(function (file) {
         if (file !== "index.js") {
