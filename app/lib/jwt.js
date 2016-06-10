9e755462bc17f1b92d8f110823f3e0f3d6d440bc
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-10 11:53:16 +0200

    Création d'une petite 'lib' pour gérer l'authentification par token. Merci jwt, ca fonctionne bien.

---
+++ app/lib/jwt.js
@@ -0,0 +1,36 @@
+var jwt = require('jsonwebtoken');
+var config = require('../../config');
+
+module.exports = {
+    newToken: function (data) {
+        var payload = {
+            username: data.username,
+            email: data.email
+        };
+
+        var token = jwt.sign(payload, config.secret, {
+                expiresIn: '24h'
+        });
+        return token;
+    },
+    checkToken: function (req, res, next) {
+        var token = req.body.token || req.query.token;
+        if(!token) {
+            res.status(403).send({
+                error: "Token missing !",
+                data: null
+            });
+        } else {
+            jwt.verify(token, config.secret, function (err, decoded) {
+                if (err) {
+                    res.status(403).send({
+                        error: "Your token is invalid.",
+                        data: null
+                    });
+                } else {
+                    next();
+                }
+            });
+        }
+    }
+};
