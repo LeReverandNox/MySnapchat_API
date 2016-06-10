7c5e9fca1502b18ddabe9c123184798bcdc28093
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-10 02:00:39 +0200

    Écriture d'un premier jet du routing /users, avec a peu pres toute les routes demandées avec les bons verbes. et un fake middleware toto pour tester :)

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,3 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../controllers/User.js');
+
+var toto = function (req, res, next) {
+    if (req.query.sexe === "bite") {
+        next();
+    } else {
+        res.status(403).send({
+            error: "Bad token",
+            data: null
+        });
+    }
+};
+
 module.exports = function (app) {
-    console.log("Routes Users");
+    router.get('/', toto, User.all);
+    router.post('/login', User.login);
+    router.post('/register', User.register);
+    router.get('/friends', User.myFriends);
+    router.post('/friends', User.addFriend);
+    router.get('/friends/requests', User.friendRequests);
+    router.delete('/friends/:friend_id', User.deleteFriend);
+    router.get('/:user_id', User.oneUser);
+    router.put('/:user_id', User.changePassword);
+
+    app.use('/api/users', router);
 };
