e8e112835789b1dc39121dd20c3820b9b45bd9a4
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-11 16:12:05 +0200

    Création d'une petite lib pour vérifier l'image uploadé avant de passer au controller, histoire d'alléger un peu le code niveau gestion d'erreur.

---
+++ app/lib/snap-validator.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+
+module.exports = function (req, res, next) {
+    'use strict';
+    var errors = [];
+
+    if (req.files[0] !== undefined) {
+        var file = req.files[0];
+        if (file.fieldname === "file") {
+            if (file.mimetype === "image/jpeg") {
+                var ext = ".jpeg";
+                fs.renameSync(file.path, file.path + ext);
+                file.filename += ext;
+                req.file = file;
+            } else {
+                errors.push('Wrong image format (expected : jpeg).');
+            }
+        } else {
+            errors.push('Wrong fileKey (expected : file).');
+        }
+    } else {
+        errors.push('Please add a JPEG image to your snap.');
+    }
+
+    if (errors.length > 0) {
+        res.status(200).send({
+            error: errors,
+            data: null
+        });
+    } else {
+        next();
+    }
+};
