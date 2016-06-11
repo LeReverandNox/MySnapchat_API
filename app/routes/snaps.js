5b405a61da7abe82fe3e4c0d398e7e6ac2fc75c1
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-11 16:14:18 +0200

    Début du routing pour la partie /snaps de l'api.  Uniquement la route pour poster des snaps pour le moment. Utilisation de multers en  plus des autre middlewares

--- app/routes/snaps.js
+++ app/routes/snaps.js
@@ -1,3 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var Snap = require('../controllers/Snap');
+var jwt = require('../lib/jwt');
+var fieldsValidator = require('../lib/fields-validator');
+var snapValidator = require('../lib/snap-validator');
+var multer = require('multer');
+var config = require('../../config');
+
 module.exports = function (app) {
-    console.log("Routes Snaps");
+    'use strict';
+    router.post('/', multer({dest: config.upload_folder}).any(), jwt.checkToken, fieldsValidator(['email', 'u2', 'temps'], true), snapValidator, Snap.newSnap);
+
+    app.use('/api/snaps', router);
 };
