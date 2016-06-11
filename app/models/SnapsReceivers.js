25da9a6d305ee4ab317d7425ecf677b5571a8cb9
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-11 16:11:11 +0200

    Création d'un model pour la table pivot snaps_receivers, afin d'y stocker des attributs supplémentaires

---
+++ app/models/SnapsReceivers.js
@@ -0,0 +1,22 @@
+module.exports = function (sequelize, DataTypes) {
+    "use strict";
+
+    var SnapsReceivers;
+    SnapsReceivers = sequelize.define("SnapsReceivers", {
+        viewed: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            get: function () {
+                return this.getDataValue('viewed');
+            },
+            set: function (viewed) {
+                this.setDataValue('viewed', viewed.toString());
+            }
+        }
+    }, {
+        timestamps: true,
+        underscored: true,
+        tableName: 'snaps_receivers'
+    });
+    return SnapsReceivers;
+};
