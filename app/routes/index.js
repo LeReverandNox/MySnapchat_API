f0930c4da70f9a05c8060c85d188ae42bcd19929
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-11 16:12:40 +0200

    Rajout du CROS dans l'index du routing, et également une route static afin de pouvoir servir les images des snaps depuis le dossier uploads.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,19 @@
 var fs = require('fs');
+var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var CROS = require('../lib/CROS');
+var config = require('../../config');
 
 module.exports = function (app) {
     "use strict";
-
+    app.use(CROS);
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
     app.use(morgan('dev'));
+
+    // Route static pour accéder aux images des snaps
+    app.use('/uploads', express.static(config.upload_folder));
 
     fs.readdirSync(__dirname).forEach(function (file) {
         if (file !== "index.js") {
