9be63af0780cb84c76906995b0a98f3a61d8dc5a
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-15 19:42:14 +0200

    Création d'un model Friends, pour gérer le MtM avec attributs. (validated)

---
+++ app/models/Friends.js
@@ -0,0 +1,22 @@
+module.exports = function (sequelize, DataTypes) {
+    "use strict";
+
+    var Friends;
+    Friends = sequelize.define("Friends", {
+        validated: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            get: function () {
+                return this.getDataValue('validated');
+            },
+            set: function (validated) {
+                this.setDataValue('validated', validated.toString());
+            }
+        }
+    }, {
+        timestamps: true,
+        underscored: true,
+        tableName: 'friends'
+    });
+    return Friends;
+};
