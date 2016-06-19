3718418e31e32824d3babbc87c135e197775ac17
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-19 17:17:48 +0200

    Validation JSLint de l'index des models

--- app/models/index.js
+++ app/models/index.js
@@ -1,19 +1,23 @@
-var fs = require('fs');
+/*jslint browser: true node: true this*/
+
+var fs = require("fs");
 var env = process.env.NODE_ENV || "development";
-var config = require('../../config')[env];
-var Sequelize = require('sequelize');
+var config = require("../../config")[env];
+var Sequelize = require("sequelize");
 var db = {};
 
 var sequelize = new Sequelize(config.database, config.username, config.password, {"host": config.host, "port": config.port});
 
 fs.readdirSync(__dirname).forEach(function (file) {
-    if (file.slice(-3) === '.js' && file !== "index.js") {
-        var model = sequelize.import('./' + file);
+    "use strict";
+    if (file.slice(-3) === ".js" && file !== "index.js") {
+        var model = sequelize.import("./" + file);
         db[model.name] = model;
     }
 });
 
 Object.keys(db).forEach(function (modelName) {
+    "use strict";
     if (db[modelName].associate !== undefined) {
         db[modelName].associate(db);
     }
