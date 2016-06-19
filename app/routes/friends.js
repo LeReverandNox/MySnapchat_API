2ea0d17b56f34c7edb40f5bb7ccf2649c2098298
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-19 17:20:35 +0200

    Validation JSLint des routes friends

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -1,18 +1,20 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../controllers/User.js');
-var jwt = require('../lib/jwt');
-var fieldsValidator = require('../lib/fields-validator');
+/*jslint browser: true node: true*/
+
+var express = require("express");
+var router = new express.Router();
+var User = require("../controllers/User.js");
+var jwt = require("../lib/jwt");
+var fieldsValidator = require("../lib/fields-validator");
 
 module.exports = function (app) {
-    'use strict';
-    router.route('/')
+    "use strict";
+    router.route("/")
         .get(jwt.checkToken, User.myFriends)
-        .post(jwt.checkToken, fieldsValidator(['email'], true), User.addFriend);
-    router.get('/requests', jwt.checkToken, User.friendRequests);
-    router.route('/requests/:user_id')
+        .post(jwt.checkToken, fieldsValidator(["email"], true), User.addFriend);
+    router.get("/requests", jwt.checkToken, User.friendRequests);
+    router.route("/requests/:user_id")
         .patch(jwt.checkToken, User.validRequest)
         .delete(jwt.checkToken, User.denyRequest);
-    router.delete('/:friend_id', jwt.checkToken, User.deleteFriend);
-    app.use('/api/friends', router);
+    router.delete("/:friend_id", jwt.checkToken, User.deleteFriend);
+    app.use("/api/friends", router);
 };
