e42ec716a42b7268cb02f04d4264b2c5ea3d3b8f
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-19 17:22:12 +0200

    Validation JSLint des routes snaps

--- app/routes/snaps.js
+++ app/routes/snaps.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var router = express.Router();
-var Snap = require('../controllers/Snap');
-var jwt = require('../lib/jwt');
-var fieldsValidator = require('../lib/fields-validator');
-var snapValidator = require('../lib/snap-validator');
-var multer = require('multer');
-var config = require('../../config');
+/*jslint browser: true node: true*/
+
+var express = require("express");
+var router = new express.Router();
+var Snap = require("../controllers/Snap");
+var jwt = require("../lib/jwt");
+var fieldsValidator = require("../lib/fields-validator");
+var snapValidator = require("../lib/snap-validator");
+var multer = require("multer");
+var config = require("../../config");
 
 module.exports = function (app) {
-    'use strict';
-    router.route('/')
-        .get(jwt.checkToken, fieldsValidator(['email'], true), Snap.mySnaps)
-        .post(multer({dest: config.upload_folder}).any(), jwt.checkToken, fieldsValidator(['email', 'u2', 'temps'], true), snapValidator, Snap.newSnap);
+    "use strict";
+    router.route("/")
+        .get(jwt.checkToken, fieldsValidator(["email"], true), Snap.mySnaps)
+        .post(multer({dest: config.upload_folder}).any(), jwt.checkToken, fieldsValidator(["email", "u2", "temps"], true), snapValidator, Snap.newSnap);
 
-    router.route('/:snap_id')
-        .patch(jwt.checkToken, fieldsValidator(['email'], true), Snap.markAsViewed);
+    router.route("/:snap_id")
+        .patch(jwt.checkToken, fieldsValidator(["email"], true), Snap.markAsViewed);
 
-    app.use('/api/snaps', router);
+    app.use("/api/snaps", router);
 };
