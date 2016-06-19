fda8abde821ea32c948f654b34819740fcce3237
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-19 16:41:40 +0200

    Validation JSLint du model Friends

--- app/models/Friends.js
+++ app/models/Friends.js
@@ -1,22 +1,23 @@
+/*jslint browser: true node: true this*/
+
 module.exports = function (sequelize, DataTypes) {
     "use strict";
-
     var Friends;
     Friends = sequelize.define("Friends", {
         validated: {
             type: DataTypes.BOOLEAN,
             allowNull: false,
             get: function () {
-                return this.getDataValue('validated');
+                return this.getDataValue("validated");
             },
             set: function (validated) {
-                this.setDataValue('validated', validated.toString());
+                this.setDataValue("validated", validated.toString());
             }
         }
     }, {
         timestamps: true,
         underscored: true,
-        tableName: 'friends'
+        tableName: "friends"
     });
     return Friends;
 };
