40597ef8cd878316bb05a9179638e1d8f336da8a
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-19 17:22:57 +0200

    Validation JSLint des routes users

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../controllers/User.js');
-var jwt = require('../lib/jwt');
-var fieldsValidator = require('../lib/fields-validator');
+/*jslint browser: true node: true*/
+
+var express = require("express");
+var router = new express.Router();
+var User = require("../controllers/User.js");
+var jwt = require("../lib/jwt");
+var fieldsValidator = require("../lib/fields-validator");
 
 module.exports = function (app) {
-    'use strict';
-    router.get('/', jwt.checkToken, User.all);
-    router.post('/login', fieldsValidator(['email', 'password'], true), User.login);
-    router.post('/register', fieldsValidator(['email', 'password', 'username'], true), User.register);
-    router.route('/:user_id')
+    "use strict";
+    router.get("/", jwt.checkToken, User.all);
+    router.post("/login", fieldsValidator(["email", "password"], true), User.login);
+    router.post("/register", fieldsValidator(["email", "password", "username"], true), User.register);
+    router.route("/:user_id")
         .get(jwt.checkToken, User.oneUser)
-        .patch(jwt.checkToken, fieldsValidator(['password'], true), User.changePassword);
-    app.use('/api/users', router);
+        .patch(jwt.checkToken, fieldsValidator(["password"], true), User.changePassword);
+    app.use("/api/users", router);
 };
